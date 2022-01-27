fb5643ee484905a5ef9c1658d0a615b85a39d48e
Megan Parsons <meganparsons@alumni.harvard.edu>
2022-01-27 03:27:37 -0500

    Update count_usa.sh

--- results/characterization/usa-tweets/count_usa.sh
+++ results/characterization/usa-tweets/count_usa.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
-DATA="/projectnb/caad/meganmp/data/usa-tweets/"
+DATA="/data/usa-tweets/"
 
 cd $DATA
 
 for f in *; do
     if [ -d "$f" ]; then
-        echo $f >> /projectnb/caad/meganmp/analysis/results/characterization/usa-tweets/count-data.txt
+        echo $f >> /analysis/results/characterization/usa-tweets/count-data.txt
         cd $f
         for x in *.jsonl.gz
         do
-            unpigz -c $x | wc -l >> /projectnb/caad/meganmp/analysis/results/characterization/usa-tweets/count-data.txt
-            echo >> /projectnb/caad/meganmp/analysis/results/characterization/usa-tweets/count-data.txt
+            unpigz -c $x | wc -l >> /analysis/results/characterization/usa-tweets/count-data.txt
+            echo >> /analysis/results/characterization/usa-tweets/count-data.txt
         done
         cd $DATA
     fi
