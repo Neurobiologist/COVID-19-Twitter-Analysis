72a5fbd5fdc858240a29bdd81ba6d218f9b9a3d6
Megan Parsons <meganparsons@alumni.harvard.edu>
2022-02-24 04:28:54 -0500

    Add network functions

--- network_analysis/network_update.py
+++ network_analysis/network_update.py
@@ -26,9 +26,9 @@ def get_interactions(row):
     interactions = set()
     
     # Replies
-    #interactions.add((str(row["in_reply_to_user_id"]), str(row["in_reply_to_screen_name"])))
+    interactions.add((str(row["in_reply_to_user_id"]), str(row["in_reply_to_screen_name"])))
     # Retweets
-    #interactions.add((str(row["retweeted_id"]), str(row["retweeted_screen_name"])))
+    interactions.add((str(row["retweeted_id"]), str(row["retweeted_screen_name"])))
     # User Mentions
     interactions.add((str(row["user_mention_id"]), str(row["user_mention_screen_name"])))
     
@@ -166,33 +166,9 @@ def tweet_polarity(tweet_data):
     plt.xlim(-1, 1)
     plt.show()
     # plt.savefig('tweet_data.png')
-
-def main():
-	# Load pkl file
-    tweet_df = pd.read_pickle('usa_sentiments_df.pkl')
-    print('Loaded data.')
     
-    """
-    # Sentiment Analysis
-    tweet_df['preprocessed'] = tweet_df.apply(
-        lambda row: preprocess_tweet(row['tweet_text']), axis=1)
-    tweet_df['sentiment_score'] = tweet_df.apply(
-        lambda row: sentiment_analysis(row['preprocessed']), axis=1)
-    tweet_df['interpretation'] = tweet_df.apply(
-                            lambda row: evaluate(row['sentiment_score']), axis=1)
-    tweet_df['marker_color'] = tweet_df.apply(
-                            lambda row: mkr(row['interpretation']), axis=1)
-    
-    # Save pkl file
-    tweet_df.to_pickle('usa_sentiments_df.pkl')
-    """
-    
-    # Build Network Graph
-    network = nx.Graph()
-    colors = []
-    subnet_colors = []
-    tweet_df['marker_color'] = tweet_df['marker_color'].astype('str')
-    
+def build_network(network, tweet_df):
+    ''' Build network node by node'''
     for index, row in tweet_df.iterrows():
         user, interactions = get_interactions(row)
         user_id = user[0]
@@ -203,32 +179,50 @@ def main():
             network.add_edge(user_id, int_id, tweet_id=tweet_id)
             network.nodes[user_id]["name"] = user_name
             network.nodes[int_id]["name"] = int_name
-               
+
+def colorize_network(network, tweet_df, colors, blank_nodes):
+    ''' Add color to nodes based on sentiment'''
     for node in network:
-        if node in tweet_df['user_id'].values:
-            colors.append((tweet_df.loc[tweet_df['user_id'] == node]['marker_color']).values[0])
-        elif int(float(node)) in tweet_df['user_id'].values:
-            colors.append((tweet_df.loc[tweet_df['user_id'] == node]['marker_color']).values[0])
+        # User ID
+        if (node in tweet_df['user_id'].values) or (int(float(node)) in tweet_df['user_id'].values):
+            if tweet_df.index[tweet_df['user_id'] == node].empty:
+                blank_nodes.append(node)
+            else:
+                colors.append((tweet_df.loc[tweet_df['user_id'] == node]['marker_color']).values[0])
+        # User Mention ID
+        elif (node in tweet_df['user_mention_id'].values) or (int(float(node)) in tweet_df['user_mention_id'].values):
+            if tweet_df.index[tweet_df['user_mention_id'] == node].empty:
+                blank_nodes.append(node)
+            else:
+                colors.append((tweet_df.loc[tweet_df['user_mention_id'] == node]['marker_color']).values[0])
+        # Retweet
+        elif (node in tweet_df['retweeted_id'].values) or (int(float(node)) in tweet_df['retweeted_id'].values):
+            if tweet_df.index[tweet_df['retweeted_id'] == node].empty:
+                blank_nodes.append(node)
+            else:
+                colors.append((tweet_df.loc[tweet_df['retweeted_id'] == node]['marker_color']).values[0])
+        # Reply
+        elif (node in tweet_df['in_reply_to_user_id'].values) or (int(float(node)) in tweet_df['in_reply_to_user_id'].values):
+            if tweet_df.index[tweet_df['in_reply_to_user_id'] == node].empty:
+                blank_nodes.append(node)
+            else:
+                colors.append((tweet_df.loc[tweet_df['in_reply_to_user_id'] == node]['marker_color']).values[0])
         else:
-            colors.append("black")
+            blank_nodes.append(node)
     
-    # Identify largest subnetwork
-    subnetwork = network.subgraph(max(nx.connected_components(network), key=len))
-    
-    for node in subnetwork:
-        if node in tweet_df['user_id'].values:
-            subnet_colors.append((tweet_df.loc[tweet_df['user_id'] == node]['marker_color']).values[0])
-        elif int(float(node)) in tweet_df['user_id'].values:
-            subnet_colors.append((tweet_df.loc[tweet_df['user_id'] == node]['marker_color']).values[0])
-        else:
-            subnet_colors.append("black")    
-    
-    # Calculate degrees of each node
+def prune_network(network, blank_nodes):
+    ''' Eliminate nodes with incomplete data'''
+    for i in range(len(blank_nodes)):
+        network.remove_node(blank_nodes[i])
+        
+def summarize_networks(network, subnetwork, filename):
+    ''' Generate summary of network as txt file'''
+        # Calculate degrees of each node
     degrees = [deg for (node, deg) in network.degree()]
     sub_degrees = [deg for (node, deg) in subnetwork.degree()]
-    subnet_degrees = dict(subnetwork.degree)
-            
-    with open('network_analysis-expanded-update-noRTorUM.txt', 'w') as file_out:
+    #subnet_degrees = dict(subnetwork.degree)
+    
+    with open(filename, 'w') as file_out:
         file_out.write('Nodes = {}\n'.format(network.number_of_nodes()))
         file_out.write('Edges = {}\n'.format(network.number_of_edges()))
         file_out.write('Max Degree = {}\n'.format(np.max(degrees)))
@@ -250,14 +244,60 @@ def main():
         file_out.write('# Connected Components = {}\n'.format(nx.number_connected_components(subnetwork)))
     
 
-    plt.figure(figsize=(50,50))
-    nx.draw(network, node_color=colors)
-    plt.savefig('network-usa-update-noRToruM-color.jpg')
+    
+    
+def main():
+	# Load pkl file
+    tweet_df = pd.read_pickle('usa_sentiments_df.pkl')
+    print('Loaded data.')
+    
+    # Sentiment Analysis
+    #tweet_df['preprocessed'] = tweet_df.apply(
+    #    lambda row: preprocess_tweet(row['tweet_text']), axis=1)
+    #tweet_df['sentiment_score'] = tweet_df.apply(
+    #    lambda row: sentiment_analysis(row['preprocessed']), axis=1)
+    #tweet_df['interpretation'] = tweet_df.apply(
+    #                        lambda row: evaluate(row['sentiment_score']), axis=1)
+    tweet_df['marker_color'] = tweet_df.apply(
+                            lambda row: mkr(row['interpretation']), axis=1)
+    
+    # Save pkl file
+    tweet_df.to_pickle('usa_sentiments_df.pkl')
+    
+    # Build Network Graph
+    network = nx.Graph()
+    colors = []
+    subnet_colors = []
+    blank_nodes = []
+    subnet_blank_nodes = []
+    tweet_df['marker_color'] = tweet_df['marker_color'].astype('str')
+    
+    # Primary Network
+    build_network(network, tweet_df)
+    colorize_network(network, tweet_df, colors, blank_nodes)
+    prune_network(network, blank_nodes)
+            
+    # Largest Subnetwork   
+    subnetwork = network.subgraph(max(nx.connected_components(network), key=len))
+    colorize_network(subnetwork, tweet_df, subnet_colors, subnet_blank_nodes)
+    prune_network(subnetwork, subnet_blank_nodes)    
+    
+
+    
+    # Generate summary of network
+    summarize_networks(network, subnetwork, 'network_analysis-expanded-update-noRTorUM.txt')
+            
+    
+    #plt.figure(figsize=(50,50))
+    #pos = nx.spectral_layout(network)
+    #nx.draw(network, node_color=colors)
+    #plt.savefig('network-usa-update-spectral.jpg')
     
     plt.figure(figsize=(50,50))
-    nx.draw(subnetwork, node_color=subnet_colors,
-            node_size=[v * 1000 for v in subnet_degrees.values()])
-    plt.savefig('subnetwork-usa-update-noRToruM-color.jpg')
+    pos2 = nx.spring_layout(subnetwork)
+    nx.draw(subnetwork, pos=pos2, node_color=subnet_colors)
+            #node_size=[v * 1000 for v in subnet_degrees.values()])
+    plt.savefig('test.jpg') #'subnet_spring_um_replies.jpg')
 
 if __name__ == "__main__":
     main()
